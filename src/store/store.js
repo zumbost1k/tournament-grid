8550bd0ed4077a3d506f7ba595105e14ea63979c
zumbost1k <matusevich.mi@gmail.com>
2023-10-15 14:27:16 +0300

    delete local storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,35 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { AllUsersSlice } from '../features/allUsers';
-import storage from 'redux-persist/lib/storage';
-import {
-  persistReducer,
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
 
 const rootReducer = combineReducers({
   allUsers: AllUsersSlice.reducer,
 });
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: rootReducer,
 });
-
-export const persistor = persistStore(store);
